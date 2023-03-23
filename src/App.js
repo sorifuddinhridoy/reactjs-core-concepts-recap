7b55d2bcdf832e501923407532c65c93e85a38a0
sorifuddinhridoy <121661222+sorifuddinhridoy@users.noreply.github.com>
2023-03-23 10:44:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District name='Noakhali' special='vibag'></District>
+      <District name='Bramonbaria' special='joda akbor'></District>
+      <District name='cumilla' special='Moy na moti'></District>
     </div>
   );
+}
+const districtStyle ={
+  backgroundColor: 'yellow',
+  margin: '20px',
+  borderRadius: '20px',
+  padding: '15px'
+
+}
+function District(props){
+
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Speciality: {props.special}</p>
+    </div>
+  )
 }
 
 export default App;
